7dbaf5d456df823867a001ab79ad18891dd3ebb4
Thomas <thomas@kodifix.de>
2021-03-10 16:21:37 +0100

    ...

--- src/PersonBuilder.java
+++ src/PersonBuilder.java
@@ -1,22 +1,26 @@
 public class PersonBuilder {
 
-    Person p = new Person();
+    Person _builderPerson = new Person();
 
-    public PersonBuilder(){}
+
+    public PersonBuilder(){
+        _builderPerson = new Person();
+    }
+
 
     public PersonBuilder addName(String name) {
-        p.set_name(name);
+        _builderPerson.set_name(name);
         return this;
     }
     public PersonBuilder addGeburtstag(String geburtstag) {
-        p.set_geburtstag(geburtstag);
+        _builderPerson.set_geburtstag(geburtstag);
         return this;
     }
     public PersonBuilder addLieblingsfarbe(String lieblingsfarbe) {
-        p.set_lieblingsfarbe(lieblingsfarbe);
+        _builderPerson.set_lieblingsfarbe(lieblingsfarbe);
         return this;
     }
     public Person build(){
-        return p;
+        return _builderPerson;
     }
 }
